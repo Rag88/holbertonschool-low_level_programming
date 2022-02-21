56c6e859e950e935fb1fa4aed7204c30d0a6467c
Rag88 <4555@holbertonschool.com>
2022-02-21 22:53:21 +0000

    Print last digit8

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,10 +8,12 @@
 
 int print_last_digit(int n)
 {
-	if (n < 0)
-		n *= -1;
+	int last = n % 10
 
-	_putchar('0' + (n % 10));
+	if (last < 0)
+		last = last * -1;
 
-	return (n % 10);
+	_putchar('0' + last);
+
+	return (last);
 }
