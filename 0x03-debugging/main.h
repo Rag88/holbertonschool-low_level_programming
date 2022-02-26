f693bfe2444b61b81b80460bf855b66795550bd2
Rag88 <4555@holbertonschool.com>
2022-02-26 00:27:05 +0000

    Largest number

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,12 +1,20 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdio.h>
 /**
  * positive_or_negative - print if integer is positive, zero or negative
+ * @i: integer
  */
-
-#include <stdio.h>
-
 void positive_or_negative(int i);
+
+/**
+ * largest_number - returns the largest of 3 numbers
+ * @a: first integer
+ * @b: second integer
+ * @c: third integer
+ * Return: largest number
+ */
+int largest_number(int a, int b, int c);
 
 #endif /* MAIN_H */
