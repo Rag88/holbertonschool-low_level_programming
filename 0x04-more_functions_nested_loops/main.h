ef76394accef53e74d14e9240b8051fee1fc0b23
Rag88 <4555@holbertonschool.com>
2022-02-27 08:17:23 +0000

    Add: Main file

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,18 @@
+#define MAIN_H
+
+/**
+ * _putchar - puts character to standard output
+ * @c: character to put to standard output
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+
+int _putchar(char c);
+
+/**
+ * _isupper - checks for uppercase character
+ * @c: character to be checked
+ * Return: 1 if true, 0 if false
+ */
+
+int _isupper(int c);
