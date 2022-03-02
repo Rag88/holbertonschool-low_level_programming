ac695e8067f441e7599a70f8d29b728af098fbf5
Rag88 <4555@holbertonschool.com>
2022-03-02 05:54:25 +0000

    Swap

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -10,10 +10,19 @@ int _putchar(char c);
 
 
 /**
- * reset_to_98 - takes a pointer to an int as parameter and updates the value it points to to 98
+ * reset_to_98 - takes a pointer to an int as parameter
+ * and updates the value it points to 98
  * @n: pointer to int n
  */
 
 void reset_to_98(int *n);
+
+/**
+ * swap_int - swaps the values of two integers
+ * @a: integer to swap
+ * @b: integer to swap
+ */
+
+void swap_int(int *a, int *b);
 
 #endif
