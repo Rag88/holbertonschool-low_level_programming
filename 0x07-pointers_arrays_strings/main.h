4ca775474b1eb3d49aa2f8f52e6d1325af6abc3e
Rag88 <4555@holbertonschool.com>
2022-03-10 05:19:10 +0000

    String search

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -29,4 +29,13 @@ char *_memset(char *s, char b, unsigned int n);
 
 char *_memcpy(char *dest, char *src, unsigned int n);
 
+/**
+ * _strchr - locate 1st occurrence of char in string and returns pointer there
+ * @s: string to search
+ * @c: target character
+ * Return: pointer to that character in string
+ */
+
+char *_strchr(char *s, char c);
+
 #endif
