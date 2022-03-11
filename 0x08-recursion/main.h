2fe11a8b84fa66303b65c85d2826ef10c809753b
Rag88 <4555@holbertonschool.com>
2022-03-11 06:49:46 +0000

    Puts with recursion

---
+++ 0x08-recursion/main.h
@@ -0,0 +1,19 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * _putchar - puts character to standard output
+ * @c: character to put to standard output
+ * Return: 1
+ */
+
+int _putchar(char c);
+
+/**
+ * _puts_recursion - print string with new line
+ * @s: string to print
+ */
+
+void _puts_recursion(char *s);
+
+#endif
