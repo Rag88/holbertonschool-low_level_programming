7b3a082349e8753065a4c574821c58d0f8b243a0
Rag88 <4555@holbertonschool.com>
2022-03-12 23:49:29 +0000

    Prime number

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -56,4 +56,12 @@ int _pow_recursion(int x, int y);
 
 int _sqrt_recursion(int n);
 
+/**
+ * is_prime_number - check if prime
+ * @n: number to check
+ * Return: 1 if prime, 0 if not
+ */
+
+int is_prime_number(int n);
+
 #endif
