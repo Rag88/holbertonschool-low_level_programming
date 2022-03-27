1e3f0cff8f4facf1802d5537d1bec8613da4ab62
Rag88 <4555@holbertonschool.com>
2022-03-27 02:46:31 +0000

    Goal Function pointer

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,30 @@
+#include "3-calc.h"
+
+/**
+ * get_op_func - function that selects the correct function to
+ * perform the operation asked by the user
+ * @s: operation given (+, -, *, /, %)
+ * Return: pointer to correct operation function (0-4)
+ */
+
+int (*get_op_func(char *s))(int, int)
+{
+	int i = 0;
+	op_t ops[] = {
+        	{"+", op_add},
+        	{"-", op_sub},
+        	{"*", op_mul},
+        	{"/", op_div},
+        	{"%", op_mod},
+        	{NULL, NULL}
+    	};
+
+	while (i < 5)
+	{
+		if (*s == *ops[i].op && s[1] == 0)
+			return (ops[i].f);
+		i++;
+	}
+	printf("Error\n");
+	exit(99);
+}
---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,27 @@
+#include "3-calc.h"
+
+/**
+ * main - program that performs simple operations
+ * @argc: number of arguments
+ * @argv: the array conteined in each position
+ * Return: always 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int num1 = 0, num2 = 0;
+
+	if (argc == 4)
+	{
+		num1 = atoi(argv[1]);
+		num2 = atoi(argv[3]);
+
+		printf("%d\n", get_op_func(argv[2])(num1, num2));
+	}
+	else
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	return (0);
+}
