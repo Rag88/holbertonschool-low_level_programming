3b7168911b02a7e0f14ac23669d0d489e6eb5210
Rag88 <4555@holbertonschool.com>
2022-04-03 06:32:19 +0000

    Fibonacci 98

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - code to print the first 50 Fibonacci Numbers
+ * Return: 0
+ */
+int main(void)
+{
+	long i;
+	int n = 98;
+	long t1 = 1, t2 = 2;
+	unsigned long long nextTerm = t1 + t2;
+
+	printf("%li, %li, ", t1, t2);
+
+	for (i = 3; i <= n; ++i)
+	{
+		if (i < n)
+		{
+			printf("%llu, ", nextTerm);
+			t1 = t2;
+			t2 = nextTerm;
+			nextTerm = t1 + t2;
+		}
+		else
+			printf("%llu", nextTerm);
+	}
+	printf("\n");
+	return (0);
+}
