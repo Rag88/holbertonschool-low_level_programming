37c54468be9e9f02640a9f26c2ba2c718ab48847
Rag88 <4555@holbertonschool.com>
2022-04-04 05:11:55 +0000

    Binary to Uint

---
+++ 0x14-bit_manipulation/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdlib.h> /* NULL */
+
+/* helper function */
+int _puchar(char c);
+
+/* function prototypes for all files */
+unsigned int binary_to_uint(const char *b);
+
+#endif
